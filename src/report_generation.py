4de06324c860ea4529c1ce726ad09b48b40857cb
Santanu <sandutta2020@gmail.com>
2022-11-03 01:56:19 -0200

    adding report

--- src/report_generation.py
+++ src/report_generation.py
@@ -1,1 +1,8 @@
 import matplotlib.pyplot as plt
+from db_connector import SqliteDB_Connect
+import pandas as pd
+class report_generation(SqliteDB_Connect):
+    def __init__(self):
+        super().__init__()
+    def report_generation_func():
+        pass
